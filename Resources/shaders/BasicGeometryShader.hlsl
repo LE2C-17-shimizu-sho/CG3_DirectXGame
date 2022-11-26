0a55a49151b368e3658312d7fbebf7f728615056
翔 清水 <k021g1379@g.neec.ac.jp>
2022-11-27 01:10:37 +0900

    変数名修正

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,21 +1,5 @@
 #include "BasicShaderHeader.hlsli"
 
-//[maxvertexcount(3)]
-//void main(
-//	triangle VSOutput input[3] : SV_POSITION,
-//	inout TriangleStream<GSOutput> output
-//)
-//{
-//    for (uint i = 0; i < 3; i++)
-//    {
-//        GSOutput element;
-//        element.svpos = input[i].svpos;
-//        element.normal = input[i].normal;
-//        element.uv = input[i].uv;
-//        output.Append(element);
-//    }
-//}
-   
 [maxvertexcount(3)]
 void main(
 	point VSOutput input[1] : SV_POSITION,
@@ -27,13 +11,13 @@ void main(
     element.normal = input[0].normal; // 法線をコピー
     element.uv = input[0].uv * 2.0f; // uvをコピー
     // 1点目
-    element.svpos = input[0].svpos; // 頂点座標をコピー
+    element.svpos = input[0].pos; // 頂点座標をコピー
     output.Append(element);
     // 2点目
-    element.svpos = input[0].svpos + float4(10.0f, 10.0f, 0, 0); // 頂点座標をコピー
+    element.svpos = input[0].pos + float4(10.0f, 10.0f, 0, 0); // 頂点座標をコピー
     output.Append(element);
     // 3点目
-    element.svpos = input[0].svpos + float4(10.0f, 0, 0, 0); // 頂点座標をコピー
+    element.svpos = input[0].pos + float4(10.0f, 0, 0, 0); // 頂点座標をコピー
     output.Append(element);
 }
 
--- Resources/shaders/BasicShaderHeader.hlsli
+++ Resources/shaders/BasicShaderHeader.hlsli
@@ -4,17 +4,17 @@ cbuffer cbuff0 : register(b0)
 	matrix mat; // ３Ｄ変換行列
 };
 
+// 頂点シェーダーからピクセルシェーダーへのやり取りに使用する構造体
+struct VSOutput
+{
+	float4 pos : POSITION; // システム用頂点座標
+	float3 normal :NORMAL; // 法線ベクトル
+	float2 uv  :TEXCOORD; // uv値
+};
+
 struct GSOutput
 {
     float4 svpos : SV_POSITION; // システム用頂点座標
     float3 normal : NORMAL; // 法線ベクトル
     float2 uv : TEXCOORD; // uv値
-};
-
-// 頂点シェーダーからピクセルシェーダーへのやり取りに使用する構造体
-struct VSOutput
-{
-	float4 svpos : SV_POSITION; // システム用頂点座標
-	float3 normal :NORMAL; // 法線ベクトル
-	float2 uv  :TEXCOORD; // uv値
 };
